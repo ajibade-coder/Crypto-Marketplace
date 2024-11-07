b9cd95ff9653adbe07f1f642320882efb91a846b
ajibade-coder <meriesbells1996@gmail.com>
2024-11-07 12:36:51 +0100

    cypto ptoject

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Copyrigth © 2024, Crypto-solution All right reserved.</p>
+        </div>
+  )
+}
+
+export default Footer
