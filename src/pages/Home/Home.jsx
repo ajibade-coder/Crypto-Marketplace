9a3fd659485a9b1701853435a9ccbab1c28c8205
ajibade-coder <meriesbells1996@gmail.com>
2025-01-11 16:12:43 +0100

    cointext -fix2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import './Home.css'
-import { CoinContext } from '../../context/koinContext'
+import { CoinContext } from '../../context/coinContext'
 
 import { Link } from 'react-router-dom'
 
